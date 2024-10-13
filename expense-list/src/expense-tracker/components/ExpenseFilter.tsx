1aa6e458151e3cdfe6d245ea4d9d60668948e419
Mark Bartolo <6192527+lavarius@users.noreply.github.com>
2024-10-12 22:14:27 -0700

    Add prop to filter for category selection

--- expense-list/src/expense-tracker/components/ExpenseFilter.tsx
+++ expense-list/src/expense-tracker/components/ExpenseFilter.tsx
@@ -4,9 +4,12 @@ interface Props {
   onSelectCategory: (category: string) => void;
 }
 
-const ExpenseFilter = () => {
+const ExpenseFilter = ({ onSelectCategory }: Props) => {
   return (
-    <select className="form-select">
+    <select
+      className="form-select"
+      onChange={(event) => onSelectCategory(event.target.value)}
+    >
       <option value="">All categories</option>
       <option value="Groceries">Groceries</option>
       <option value="Utilites">Utilities</option>
